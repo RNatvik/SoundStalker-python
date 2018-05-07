177f06a2dced5128b0ad77b4fd193c0a0c039158
RNatvik <r_bn-95@hotmail.com>
2018-05-07 17:39:03 +0200

    testing RPi.GPIO nothing significant added

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,17 @@
 from pyfirmata import Arduino, util
 from src.ArduinoCom import BatterySensor, TempSensor
+from src.PiSensors import SonicSensor
+import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
 
 board = Arduino('/dev/ttyUSB0')
 batterySensor = BatterySensor(board, 'a:0:i')
 tempSensor = TempSensor(board, 'a:1:i')
+
+sonic = SonicSensor()
+
 
 time.sleep(2)
 
@@ -15,3 +21,6 @@ print(batterySensor.isFullyCharged())
 print(batterySensor.isDepleted())
 
 board.exit()
+
+while True:
+    sonic.shitTestForRuben()
--- src/PiSensors.py
+++ src/PiSensors.py
@@ -4,4 +4,22 @@ import RPi.GPIO as GPIO
 class SonicSensor:
 
     def __init__(self):
+        self.triggerPin = 31
+        self.echoPin = 32
+        GPIO.setup(self.triggerPin, GPIO.OUT)
+        GPIO.setup(self.echoPin, GPIO.IN)
+
+    def trigger(self):
         pass
+
+    def waitForEcho(self):
+        pass
+
+    def getDistance(self):
+        self.trigger()
+
+    def shitTestForRuben(self):
+        if GPIO.input(self.echoPin):
+            GPIO.output(self.triggerPin, 1)
+        else:
+            GPIO.output(self.triggerPin, 0)
