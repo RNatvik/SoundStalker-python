34827f0388a7e1415564e847d031eed0e622a468
ruben-Ubuntu <itzRuben96@gmail.com>
2018-05-07 16:18:57 +0200

    Image prossesing

--- src/ImageProssesing.py
+++ src/ImageProssesing.py
@@ -5,12 +5,18 @@ import time
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # allow the camera to warmup
 time.sleep(0.1)
 
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
