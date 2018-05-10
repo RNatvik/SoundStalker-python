54598cd89ab2970c883dad5c093609ee0fc0ce59
ruben-Ubuntu <itzRuben96@gmail.com>
2018-05-10 17:46:43 +0200

    imageProssesing

--- src/ImageProssesing.py
+++ src/ImageProssesing.py
@@ -1,5 +1,6 @@
 # import the necessary packages
-import numpy as numpy
+import cv2
+import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -19,12 +20,25 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # and occupied/unoccupied text
     image = frame.array
 
-    redPixsels =numpy.add                                              
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 2, 32, param1=200, param2=100)
 
-    print(image)
-    print(image.shape)
-    print(image.ndim)
-    print("\n\n\n\n\n\n\n\n\n\n")
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        print(type(image))
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the centre of the circle
+            cv2.circle(image, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(image, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+        # show the output image
+        cv2.imshow("output", np.hstack([image]))
+    cv2.waitKey(0)
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
